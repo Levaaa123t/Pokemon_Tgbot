10121313c52307f5051916bba7266fa5dbc368c4
Levaaa123t <143887693+Levaaa123t@users.noreply.github.com>
2024-10-11 20:07:13 +0200

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,10 +1,11 @@
 from random import randint
 import requests
 
+
 class Pokemon:
     pokemons = {}
     # Инициализация объекта (конструктор)
-    def __init__(self, pokemon_trainer, level = 0, feed = 0):
+    def __init__(self, pokemon_trainer, level = 0, feed = 1):
 
         self.pokemon_trainer = pokemon_trainer   
         self.pokemon_number = randint(1,1000)
@@ -12,6 +13,8 @@ class Pokemon:
         self.name = self.get_name()
         self.feed = feed
         self.level = level
+        self.hp = randint(50, 100)
+        self.power = randint(10,20)
         Pokemon.pokemons[pokemon_trainer] = self
     # Метод для получения картинки покемона через API
     def get_img(self):
@@ -33,23 +36,56 @@ class Pokemon:
         else:
             return "Pikachu"
     def give_food(self):
-        self.feed +=  1
-        self.level += 0.5
-        return f'Вы успешно покормили своего покемона! Количетсво раз сколько его кормили: {self.feed}, его уровень: {self.level}'
+        if self.feed <= 1:    
+            self.feed -=  1
+            self.level += 0.5
+            self.hp += randint(5,10)
+            return f'Вы успешно покормили своего покемона! Количество еды для покемона: {self.feed}, его уровень: {self.level}, его здоровье:{self.hp}'
         #elif self.feed <= 1:
          #   self.feed += 1
           #  self.level += 0.5
+        else:
+            return'У вас нет доступной еды для покемона!'
 
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покемона: {self.name}, его уровень: {self.level}, количество раз сколько его кормили: {self.feed}"
-        
+        return f"""Имя твоего покемона: {self.name}
+    Уровень твоего покемона: {self.level}
+    Количество еды для покемона: {self.feed}
+    Количество жизней:{self.hp}
+    Сила покемона: {self.power}
+"""
+    def attack(self, enemy):
+        if isinstance(enemy, Wizard): # Проверка на то, что enemy является типом данных Wizard (является экземпляром класса Волшебник)
+            chance2 = randint(1,5)
+            if chance2 == 1 or 2:
+                return "Покемон-волшебник применил щит в сражении"
+        elif enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"""Сражение {self.pokemon_trainer} с {enemy.pokemon_trainer}
+Здоровье покемона @{self.pokemon_trainer}:{self.hp}
+Здоровье покеомна @{enemy.pokemon_trainer}: {enemy.hp}
+"""
+        else:
+            enemy.hp = 0
+            return f"Победа {self.pokemon_trainer} над {enemy.pokemon_trainer}! "
     def about_level(self):
-        return f'Уровень твоего покемона: {self.level}'
+        self.level += 1
+        return f'Уровень твоего покемона: {self.level}, количество еды для покемона:{self.feed}'
+        
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
 
+class Wizard(Pokemon):
+    pass
 
+class Fighter(Pokemon):
+    def attack(self, enemy):
+        super_power = randint(5,15)
+        self.power += super_power
+        result = super().attack(enemy)
+        self.power -= super_power
+        return result + f"\nБоец применил супер-атаку силой:{super_power} "
 
 
