dfff0dcf43514244149f47f633dd39bc2fa243a2
Levaaa123t <143887693+Levaaa123t@users.noreply.github.com>
2024-10-18 22:21:08 +0200

    Update logic.py

--- logic.py
+++ logic.py
@@ -7,17 +7,17 @@ from datetime import datetime, timedelta
 class Pokemon:
     pokemons = {}
     # Инициализация объекта (конструктор)
-    def __init__(self, pokemon_trainer, level = 0, feed = 1):
+    def __init__(self, pokemon_trainer, level = 0, count_feed = 1):
 
         self.pokemon_trainer = pokemon_trainer   
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
-        self.feed = feed
+        self.count_feed = count_feed
         self.level = level
         self.hp = randint(50, 100)
         self.power = randint(10,20)
-        self.last_feed_time = datetime.now
+        self.last_feed_time = datetime.now()
         Pokemon.pokemons[pokemon_trainer] = self
     # Метод для получения картинки покемона через API
     def get_img(self):
@@ -38,32 +38,34 @@ class Pokemon:
             return (data['forms'][0]['name'])
         else:
             return "Pikachu"
-    def give_food(self):
-        if self.feed >= 1:    
-            self.feed -=  1
-            self.level += 0.5
-            self.hp += randint(5,10)
-            return f'Вы успешно покормили своего покемона! Количество еды для покемона: {self.feed}, его уровень: {self.level}, его здоровье:{self.hp}'
-        else:
-            return'У вас нет доступной еды для покемона!'
+    #def give_food(self):
+     #   if :    
+      #      
+        #    self.hp += randint(5,10)
+         #   return f'Вы успешно покормили своего покемона! Количество еды для покемона: {self.feed}, его уровень: {self.level}, его здоровье:{self.hp}'
+        #else:
+         #   return'У вас нет доступной еды для покемона!'
         
-    def feed3(self, feed_interval = 20, hp_increase = 10 ):
+    def feed3(self, feed_interval = 20, hp_increase = randint(10,15) ):
         current_time = datetime.now()
         delta_time = timedelta(seconds=feed_interval)  
-        if (current_time - self.last_feed_time) > delta_time:
+        if (current_time - self.last_feed_time) > delta_time and self.count_feed >= 1:
+            self.count_feed -=  1
+            self.level += 0.5
             self.hp += hp_increase
             self.last_feed_time = current_time
-            return f"Здоровье покемона увеличено. Текущее здоровье: {self.hp}"
-        
+            return f"Здоровье покемона увеличено. Текущее здоровье: {self.hp}, текущий уровень: {self.level}, количество еды для покемона: {self.count_feed}"
         else:
-            return f"Следующее время кормления покемона: {current_time+delta_time}"  
+            return f"""Вы не можете покормить вашего покемона если с последнего кормления не прошло 20 секунд и у вас нет еды.
+        Следующее время кормления покемона: {current_time+delta_time}, количетсво еды для покемона: {self.count_feed}
+        """ 
             
 
     # Метод класса для получения информации
     def info(self):
         return f"""Имя твоего покемона: {self.name}
     Уровень твоего покемона: {self.level}
-    Количество еды для покемона: {self.feed}
+    Количество еды для покемона: {self.count_feed}
     Количество жизней:{self.hp}
     Сила покемона: {self.power}
 """
@@ -74,25 +76,28 @@ class Pokemon:
             #self.power = randint(5,15)
             if chance2 >= 7:
                 return f"Покемон-волшебник({enemy.pokemon_trainer}) применил щит в сражении"
+        if self.hp <= 0:
+                return f'{self.pokemon_trainer} Не может атаковать так как имеет 0 здоровья!'
         if enemy.hp > self.power:
             enemy.hp -= self.power
+            if enemy.hp <= 0:
+                self.count_feed = 1
+                self.level = 2
+                return f"Победа {self.pokemon_trainer} над {enemy.pokemon_trainer}! Ваш уровень = {self.level}, ваше количество еды = {self.count_feed}"
+            
             return f"""Сражение {self.pokemon_trainer} с {enemy.pokemon_trainer}
-Здоровье покемона @{self.pokemon_trainer}:{self.hp}
-Здоровье покеомна @{enemy.pokemon_trainer}: {enemy.hp}
+Здоровье покемона {self.pokemon_trainer}:{self.hp}
+Здоровье покемона {enemy.pokemon_trainer}: {enemy.hp}
 """
-        else:
-            enemy.hp = 0
-            return f"Победа {self.pokemon_trainer} над {enemy.pokemon_trainer}! "
     def about_level(self):
-        self.level += 1
-        return f'Уровень твоего покемона: {self.level}, количество еды для покемона:{self.feed}'
+        return f'Уровень твоего покемона: {self.level}, количество еды для покемона:{self.count_feed}'
         
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
 
 class Wizard(Pokemon):
-    def feed3(self):
+    def feed_wizzard(self):
         super().feed3(feed_interval=10)
 
 class Fighter(Pokemon):
@@ -104,7 +109,8 @@ class Fighter(Pokemon):
         result = super().attack(enemy)
         self.power -= super_power
         return result + f"\nБоец применил супер-атаку силой:{super_power} "
-    def feed3(self):
+    def feed_fighter(self):
         super().feed3(hp_increase=20)
+
 
 
